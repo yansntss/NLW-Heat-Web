806adee5f07005981935f21de9a55b037a1c7748
yansntss <yansantos.dev@gmail.com>
2021-10-20 23:24:25 -0300

    :sparkles: Home page
    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styles from "./App.module.scss";
 import { LoginBox } from "./components/LoginBox";
-import { MessageList } from "./components/MassageList";
+import { MessageList } from "./components/MessageList";
 
 export function App() {
   return (
---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,50 @@
+import styles from "./styles.module.scss";
+import logoImg from "../../assets/logo.svg";
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/yansntss.png" alt="yansntss" />
+            </div>
+            <span>yansntss</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/yansntss.png" alt="yansntss" />
+            </div>
+            <span>yansntss</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/yansntss.png" alt="yansntss" />
+            </div>
+            <span>yansntss</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
