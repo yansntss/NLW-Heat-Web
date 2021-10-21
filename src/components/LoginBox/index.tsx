ffd09132ad927c5aafbaf2772640552eb2e05d8e
yansntss <yansantos.dev@gmail.com>
2021-10-20 22:14:04 -0300

    :sparkles: Component LoginBox
    #

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,7 +1,15 @@
+import {VscGithubInverted} from 'react-icons/vsc'
+
+import styles from "./styles.module.scss";
+
 export function LoginBox() {
   return (
-    <div className="loginBoxWrapper">
-      
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href="#" className={styles.signInWithGithub}> 
+      <VscGithubInverted size="24px" />
+      Entrar com gitHub
+      </a>
     </div>
-  )
+  );
 }
