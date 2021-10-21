53822227240ea75f73c790c3f6ce63403b3d35b5
yansntss <yansantos.dev@gmail.com>
2021-10-21 10:04:39 -0300

    :construction: feature signIn
    saving token in localStorage

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,33 @@
+import { useEffect } from 'react';
 import {VscGithubInverted} from 'react-icons/vsc'
 
 import styles from "./styles.module.scss";
 
 export function LoginBox() {
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c2ef8d87b60b2f4fd720`
+
+
+  useEffect(()=>{
+    //pegando url da aplicação
+    const url = window.location.href
+    //verificando se no texto da url tem o code
+    const hasGithubCode = url.includes('?code=')
+
+    if(hasGithubCode) {
+      //o que vim antes do ' ?code= ' é minha url sem o code e o que vim depois, vai ser meu code
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      //tirando o code da url para nao ficar exposto 
+      window.history.pushState({}, '', urlWithoutCode)
+      
+    }
+  },[])
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}> 
+      <a href={signInUrl} className={styles.signInWithGithub}> 
       <VscGithubInverted size="24px" />
       Entrar com gitHub
       </a>
