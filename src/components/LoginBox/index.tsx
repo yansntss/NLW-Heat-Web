284ade01580e20b022ee6b43a94c785a3117a90b
yansntss <yansantos.dev@gmail.com>
2021-10-21 11:22:17 -0300

    :construction: Feature reactContext
    #

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,60 +1,19 @@
-import { useEffect } from 'react';
-import {VscGithubInverted} from 'react-icons/vsc'
-import { api } from '../../services/api';
-
+import { useContext, useEffect } from "react";
+import { VscGithubInverted } from "react-icons/vsc";
+import { AuthContext } from "../../context/auth";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
-type AuthResponse = {
-  token: string;
-  user: {
-    id:string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  }
-}
-
 export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext);
 
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c2ef8d87b60b2f4fd720`
-
-  async function signIn(githubCode: string){
-    const response  = await api.post<AuthResponse>('authenticate', {
-      code: githubCode,
-    })
-
-    const {token, user } = response.data;
-
-    //salvando o token no storage do navegador
-    localStorage.setItem('@dowhile:token', token)
-
-    console.log(user)
-  }
-
-
-  useEffect(()=>{
-    //pegando url da aplicação
-    const url = window.location.href
-    //verificando se no texto da url tem o code
-    const hasGithubCode = url.includes('?code=')
-
-    if(hasGithubCode) {
-      //o que vim antes do ' ?code= ' é minha url sem o code e o que vim depois, vai ser meu code
-      const [urlWithoutCode, githubCode] = url.split('?code=')
-
-      //tirando o code da url para nao ficar exposto 
-      window.history.pushState({}, '', urlWithoutCode)
-      
-      signIn(githubCode);
-    }
-  },[])
-
+  console.log(user);
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href={signInUrl} className={styles.signInWithGithub}> 
-      <VscGithubInverted size="24px" />
-      Entrar com gitHub
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24px" />
+        Entrar com gitHub
       </a>
     </div>
   );
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,14 +1,75 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
 
-const AuthContext = createContext(null);
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
 
 type AuthProvider = {
   //tipo: tudo que o react aceita
   children: ReactNode;
 };
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id:string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
 export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c2ef8d87b60b2f4fd720`
+
+  async function signIn(githubCode: string){
+    const response  = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+
+    const {token, user } = response.data;
+
+    //salvando o token no storage do navegador
+    localStorage.setItem('@dowhile:token', token)
+
+    setUser(user)
+  }
+
+  
+
+  useEffect(()=>{
+    //pegando url da aplicação
+    const url = window.location.href
+    //verificando se no texto da url tem o code
+    const hasGithubCode = url.includes('?code=')
+
+    if(hasGithubCode) {
+      //o que vim antes do ' ?code= ' é minha url sem o code e o que vim depois, vai ser meu code
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      //tirando o code da url para nao ficar exposto 
+      window.history.pushState({}, '', urlWithoutCode)
+      
+      signIn(githubCode);
+    }
+  },[])
+
   return (
-    <AuthContext.Provider value={null}>{props.children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signInUrl, user }}>
+      {props.children}
+      </AuthContext.Provider>
   );
 }
