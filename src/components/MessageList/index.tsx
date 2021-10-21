bc72b73c14202f32ced93b10b2825b4559df6931
yansntss <yansantos.dev@gmail.com>
2021-10-21 08:57:11 -0300

    :construction: connecting back-end to front-end
    #

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -18,57 +18,34 @@ type Message = {
 };
 
 export function MessageList() {
-                                  //useState é uma lista de mensagem
+  //useState é uma lista de mensagem
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     //api é uma lista de mensagem
     api.get<Message[]>("messages/last3").then((response) => {
-      setMessages(response.data)
+      setMessages(response.data);
     });
   }, []);
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/yansntss.png" alt="yansntss" />
-            </div>
-            <span>yansntss</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/yansntss.png" alt="yansntss" />
-            </div>
-            <span>yansntss</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/yansntss.png" alt="yansntss" />
-            </div>
-            <span>yansntss</span>
-          </div>
-        </li>
+        {messages.map((message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>
+                {message.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        }))}
       </ul>
     </div>
   );
